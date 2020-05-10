43afb7d20c0cb431458d0d6e2b548e96189fcbea
Thomasjuuljensen <59442863+Thomasjuuljensen@users.noreply.github.com>
2020-05-10 10:56:40 +0200

    button to create blog posts added

---
+++ src/context/BlogContext.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+    const [blogPost, setBlogPosts] = useState([]);
+
+    const addBlogPost = () => {
+        setBlogPosts([
+            ...blogPost,
+            { title: `Blog Post #${blogPost.length + 1}` },
+        ]);
+    };
+    return (
+        <BlogContext.Provider value={{ data: blogPost, addBlogPost }}>
+            {children}
+        </BlogContext.Provider>
+    );
+};
+
+export default BlogContext;
